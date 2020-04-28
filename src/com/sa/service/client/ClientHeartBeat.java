3e3c5f29b6c3afec66497c73e34c8ad9a30ebaf0
Y.P <Y.P@DESKTOP-8KU330T>
2020-04-28 19:00:57 +0800

    1

--- src/com/sa/service/client/ClientHeartBeat.java
+++ src/com/sa/service/client/ClientHeartBeat.java
@@ -2,18 +2,9 @@ package com.sa.service.client;
 
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
-import com.sa.util.ByteBufUtil;
 
 public class ClientHeartBeat extends Packet{
 	public ClientHeartBeat(){}
-
-	@Override
-	public void writePacketBody(ByteBufUtil buf) {
-	}
-
-	@Override
-	public void readPacketBody(ByteBufUtil buf) {
-	}
 
 	@Override
 	public PacketType getPacketType() {
@@ -22,12 +13,6 @@ public class ClientHeartBeat extends Packet{
 
 	@Override
 	public void execPacket() {
-System.out.println("心跳client");
-	}
-
-	@Override
-	public String toString() {
-		return null;
 	}
 
 }
